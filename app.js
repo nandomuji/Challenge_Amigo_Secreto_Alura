221def1bee7b542a80f6dd21442232f1751ab410
Hernando Muñoz <nando_mj@hotmail.com>
2025-02-28 17:32:27 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ const amigos = [];
 const inputNombreAmigo = document.getElementById('amigo');
 const listaAmigos = document.getElementById('listaAmigos');
 const resultado = document.getElementById('resultado');
+const botonReinicio = document.getElementById('reinicioBtn');
 
 // Función para agregar amigos
 const agregarAmigo = () => {
-  let nombreAmigo = inputNombreAmigo.value;
+  let nombreAmigo = inputNombreAmigo.value.trim();
   if (nombreAmigo) {
     if (amigos.includes(nombreAmigo)) {
       inputNombreAmigo.value = '';
       alert(`Ya existe un amigo llamado ${nombreAmigo}`);
       return;
     }
-    amigos.push(inputNombreAmigo.value);
+    amigos.push(nombreAmigo);
     inputNombreAmigo.value = '';
     actualizarListaAmigos();
     return;
@@ -30,24 +31,36 @@ const actualizarListaAmigos = () => {
     li.textContent = amigo;
     listaAmigos.appendChild(li);
   });
-  // Limpiar el resultado anterior
+
   if (resultado.textContent) {
     resultado.textContent = '';
   }
 }
 
 // Función para sortear amigos
+
 const sortearAmigo = () => {
-  if (amigos.length) {
-    const indiceAmigoElegido = Math.floor(Math.random() * amigos.length);
-    const amigoElegido = amigos.splice(indiceAmigoElegido, 1);;
-    listaAmigos.innerHTML = '';
-    actualizarListaAmigos();
-    resultado.innerHTML = `El amigo elegido es ${amigoElegido}`;
-  } else {
-    resultado.innerHTML = 'No hay amigos para sortear';
+  if (amigos.length < 2) {
+    resultado.innerHTML = 'Debe ingresar al menos 2 amigos para sortear.';
+    return;
   }
+
+  const indiceAmigoElegido = Math.floor(Math.random() * amigos.length);
+  const amigoElegido = amigos.splice(indiceAmigoElegido, 1)[0];
+  actualizarListaAmigos();
+ // resultado.innerHTML = `El amigo elegido es <strong>${amigoElegido}</strong>`;
+  resultado.innerHTML = `El amigo elegido es <strong class="amigo-sorteado">${amigoElegido}</strong>`;
+  // Habilitar botón de reinicio
+  botonReinicio.disabled = false;
 }
 
-
-
+// Función para limpiar todo
+const clearControl = () => {
+  amigos.length = 0;  // Vaciar el array
+  listaAmigos.innerHTML = '';
+  resultado.innerHTML = '';
+  inputNombreAmigo.value = '';
+  
+  // Deshabilitar botón de reinicio nuevamente
+  botonReinicio.disabled = true;
+}
